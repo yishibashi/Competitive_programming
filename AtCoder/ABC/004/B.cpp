d985f54a444d8832d0440a759b82ba2672bb8354
yishibashi <i.yoshihiro.mbox@gmail.com>
2016-04-28 20:41:20 +0900

    solved 'ABC_004_B'

--- AtCoder/ABC/004/B.cpp
+++ AtCoder/ABC/004/B.cpp
@@ -11,30 +11,13 @@ int main()
 
     for(int i = 0; i < 4; i++){
         getline(cin, tmp);
-        //cin.ignore();
-        //cout << " ! " << tmp << endl;
         map.push_back(tmp);
     }
-/*
-    cout << "========================" << endl;
-    cout << map[0] << endl;
-    cout << map[1] << endl;
-    cout << map[2] << endl;
-    cout << map[3] << endl;
-    cout << "========================" << endl;
-*/
-    for(int j = map.size(); j >= 0; j--){
-        for(int i = map[1].size(); i >= 0; i--){
-            cout << map[j][i] ;
+    for(int j = map.size()-1 ; j > -1; j--){
+        for(int i = map[1].size() ; i != -1; i--){
+            cout << map[j][i] << " ";
         }
         cout << endl;
     }
-/*
-    cout << "========================" << endl;
-    cout << map[0] << endl;
-    cout << map[1] << endl;
-    cout << map[2] << endl;
-    cout << map[3] << endl;
-*/
     return 0;
 }
