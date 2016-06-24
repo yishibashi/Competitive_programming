feb8b0962ba9e1e72ad766e99dd715845c1a41b7
yishibashi <yishibashi101@gmail.com>
2016-06-25 01:01:20 +0900

    can't solve in time

---
+++ ICPC/2016/domestic/C.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int m,n;
+    while(cin >> m >> n, m&&n){
+        vector<int> sieve(7400000,0);
+        for (int i = 0; i < sieve.size(); ++i) sieve[i] = i;
+        for (int i = 0; i < m; ++i) sieve[i] = 0;
+
+        int count = 1;
+        for(int j = m; j < sieve.size(); j++){99
+            if(count > n) break;
+            if(sieve[j] != 0) {
+                count++;
+                for (int k = sieve[j]; k < sieve.size(); k = k + j) {
+                    sieve[k] = 0;
+                }
+            }
+        }
+
+        for (int l = 0; l < sieve.size(); ++l) {
+            if(sieve[l] != 0){
+                cout << sieve[l] << endl;
+                break;
+            }
+        }
+
+    }
+    return 0;
+}
